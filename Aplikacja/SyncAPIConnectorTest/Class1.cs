e6a7524f124a2a968fc65c87c9a15b05b91dd822
Deloo <del19@wp.pl>
2013-01-02 18:28:51 +0100

    ap

---
+++ Aplikacja/SyncAPIConnectorTest/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using pl.xtb.api.message.command;
+using System.Net.Sockets;
+
+
+
+namespace SyncAPIConnectorTest
+{
+    class Class1
+    {
+        //samgggggdd
+        //hyghggh
+        //opaaa
+        string MakeRequest(string josnString, System.Net.Sockets.TcpClient m_socClient)
+    {
+        System.String outputSting = null;
+        try
+        {
+            String szData = josnString;
+            byte[] byteData = System.Text.Encoding.ASCII.GetBytes(szData);
+            m_socClient.Send(byteData);
+
+            byte[] buffer = new byte[8192];
+            int iRx = m_socClient.Receive(buffer);
+            string buf = System.Text.Encoding.ASCII.GetString(buffer);
+            char[] chars = new char[iRx];
+
+            System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+            int charLen = d.GetChars(buffer, 0, iRx, chars, 0);
+            outputSting = new System.String(chars);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message.ToString());
+            //if ((ex.Message.Contains("An established connection was aborted by the software in your host machine")) || (ex.Message.Contains("An existing connection was forcibly closed by the remote host")))
+            //{
+            //    lblMsg.Text = "Server error... please try after some time";
+            //    lblMsg.ForeColor = System.Drawing.Color.Red;
+            //}
+            //else
+            //{
+            //    lblMsg.Text = ex.Message;
+            //    lblMsg.ForeColor = System.Drawing.Color.Red;
+            //}
+        }
+
+        return outputSting.ToString();
+    }
+        public static void Main(string[] args){
+
+            
+            private System.Net.Sockets.TcpClient m_socClient = new System.Net.Sockets.TcpClient("195.182.34.175", 23460);
+            LoginCommand login = new LoginCommand(jsonObject, true);
+            string response = MakeRequest(login.toJSONString(), m_socClient);
+
+
+        }
+
+
+
+
+    }
+
+
+}
---
+++ Aplikacja/SyncAPIConnectorTest/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using pl.xtb.api.sync;
+using pl.xtb.api.message.command;
+using pl.xtb.api.message.response;
+using pl.xtb.api.message.error;
+using SyncAPIConnector.streaming;
+using pl.xtb.api.message.records; 
+using pl.xtb.api.streaming;
+using pl.xtb.api.message.codes;
+using System.Threading;
+
+namespace SyncAPIConnectorTest
+{
+    class StrList : StreamingListener
+    {
+        void StreamingListener.receiveTradeRecord(TradeRecord tradeRecord)
+        {
+            Console.WriteLine(tradeRecord.ToString());
+        }
+        void StreamingListener.receiveTickRecord(TickRecord tickRecord)
+        {
+            Console.WriteLine(tickRecord.ToString());
+            foreach (KeyValuePair<long?, double?> i in tickRecord.Profits)
+            {
+                Console.WriteLine(i.Key + " " + i.Value);
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                SyncAPIConnect connector = new SyncAPIConnect(ServerData.ProductionServers["CONTEST"]);
+
+                /* Enter your registration credentials (login & pass) here.	   LOGIN     PASSWORD			*/
+                BaseCommand loginCmd = APICommandFactory.createLoginCommand( 00000000, "your_password", false);
+                Console.Error.WriteLine("SENDING LoginCommand   :  " + loginCmd.toJSONString());
+                string responseJSON = connector.executeCommand(loginCmd.toJSONString());
+                Console.Error.WriteLine("RECEIVED LoginResponse   : " + responseJSON);
+                LoginResponse resp = new LoginResponse(responseJSON);
+                Console.Error.WriteLine(resp.Status);
+                
+                StrList xyz = new StrList();
+               StreamingAPIConnect scnn = new StreamingAPIConnect(xyz, ServerData.ProductionServers["REAL"], resp);
+                
+                scnn.subscribeTrades();
+                LinkedList<string> symbols = new LinkedList<string>();
+                symbols.AddLast("EURUSD.");
+                symbols.AddLast("EURJPY.");
+                symbols.AddLast("USDPLN.");
+                symbols.AddLast("EURGBP.");
+                scnn.subscribePricesL(symbols);
+
+                CurrentUserDataResponse data = APICommandFactory.executeCurrentUserDataCommand(connector, false);
+                Console.WriteLine(data.Currency + " " + data.Leverage);
+                ProfitCalculationResponse resp_calc = APICommandFactory.executeProfitCalculationCommand(connector, "EURUSD", 0.1, TRADE_OPERATION_CODE.BUY, 1.001, 1.0003, false);
+                Console.WriteLine(resp_calc.Profit);
+                MarginTradeResponse mtrade = APICommandFactory.executeMarginTradeCommand(connector, "EURUSD", 0.1, TRADE_OPERATION_CODE.BUY, false);
+                Console.WriteLine(mtrade.Margin);
+                mtrade = APICommandFactory.executeMarginTradeCommand(connector, "EURUSD", 0.1, false);
+                Console.WriteLine(mtrade.Margin);
+                mtrade = APICommandFactory.executeMarginTradeCommand(connector, "EURUSD", 0.1, TRADE_OPERATION_CODE.SELL, false);
+                Console.WriteLine(mtrade.Margin);
+                mtrade = APICommandFactory.executeMarginTradeCommand(connector, "EURUSD", 0.1, TRADE_OPERATION_CODE.BUY_LIMIT, false);
+                Console.WriteLine(mtrade.Margin);
+
+                SymbolResponse sresp = APICommandFactory.executeSymbolCommand(connector, "EURUSD", false);
+                SymbolRecord s = sresp.Symbol;
+                Console.WriteLine(s.Ask + " " + s.Bid +" " + s.ContractSize + " " + s.SwapLong);
+
+                    Console.Read();
+                connector.close();
+            }
+            catch (APICommandConstructionException ex)
+            {
+                Console.Error.WriteLine(ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.Write(ex.StackTrace);
+                Console.Read();
+            }
+            
+        }
+        
+
+    }
+}
